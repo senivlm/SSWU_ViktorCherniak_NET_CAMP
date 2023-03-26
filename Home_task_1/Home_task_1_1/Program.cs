86908e8e89c7839a6a0c1d079f37a059c1a3b6f8
Forsen-Baj <viktor.cherniak.11@gmail.com>
2023-03-26 22:48:22 +0300

    fixed issue when given incorrect matrix size

--- Home_task_1/Home_task_1_1/Program.cs
+++ Home_task_1/Home_task_1_1/Program.cs
@@ -3,8 +3,13 @@
 try
 {
     var values = Console.ReadLine().Split().Select(s => int.Parse(s)).ToList();
-    SpiralMatrix matrix = new SpiralMatrix(values[0], values[1]);
-    matrix.Print();
+    if (values[0] < 2 || values[1] < 2)
+        Console.WriteLine("Minimal size is 2x2");
+    else
+    {
+        SpiralMatrix matrix = new SpiralMatrix(values[0], values[1]);
+        matrix.Print();
+    }
 }
 catch (Exception ex)
 {
