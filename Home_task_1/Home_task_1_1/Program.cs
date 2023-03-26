10210395007aac6b04370af4223c8119172cd6d0
Forsen-Baj <viktor.cherniak.11@gmail.com>
2023-03-26 22:36:37 +0300

    task 1.1 done

---
+++ Home_task_1/Home_task_1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Home_task_1_1;
+
+try
+{
+    var values = Console.ReadLine().Split().Select(s => int.Parse(s)).ToList();
+    SpiralMatrix matrix = new SpiralMatrix(values[0], values[1]);
+    matrix.Print();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ Home_task_1/Home_task_1_1/SpiralMatrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_1_1
+{
+    public class SpiralMatrix
+    {
+        int _width;
+        int _height;
+        int[,] _matrix;
+
+        public SpiralMatrix(int height = 4, int width = 4)
+        {
+            if (height < 0 || width < 0)
+            {
+                _width = 0;
+                _height = 0;
+                _matrix = new int[0, 0];
+            }
+            else
+            {
+                _width = width;
+                _height = height;
+                _matrix = new int[width, height];
+            }
+        }
+
+        bool IsCompleted(int counter)
+        {
+            return counter > _width * _height;
+        }
+        void _generateMatrix()
+        {
+            int counter = 1;
+            int x = 0;
+            int y = 0;
+            int lengthX = _width - 1;
+            int lengthY = _height;
+
+            while (!IsCompleted(counter))
+            {
+                // Down
+                for (int i = 0; i < lengthY; i++)
+                    _matrix[x, y++] = counter++;
+
+                x++; y--; lengthY--;
+                // Right
+                for (int i = 0; i < lengthX; i++)
+                    _matrix[x++, y] = counter++;
+
+                x--; y--; lengthX--;
+                //Up
+                for (int i = 0; i < lengthY; i++)
+                    _matrix[x, y--] = counter++;
+
+                x--; y++; lengthY--;
+
+                // Left
+                for (int i = 0; i < lengthX; i++)
+                    _matrix[x--, y] = counter++;
+
+                x++; y++; lengthX--;
+            }
+        }
+
+        public void Print()
+        {
+            _generateMatrix();
+
+            for (int y = 0; y < _height; y++)
+            {
+                for(int x = 0; x < _width; x++)
+                    Console.Write(_matrix[x,y] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
